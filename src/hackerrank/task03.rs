45d773de0b0262f58d604ec25a2943e71164b60f
alaska d <stirbnichtvormirr@gmail.com>
2026-02-22 21:38:43 +0200

    added staircase hackerrank result screenshot

--- src/hackerrank/task03.rs
+++ src/hackerrank/task03.rs
@@ -1,13 +1,51 @@
 use std::io;
 
-fn main() 
+fn build_staircase(n: usize) -> Vec<String> 
+{
+    let mut result = Vec::new();
+    for i in 1..=n 
+    {
+        result.push(format!("{:>width$}", "#".repeat(i), width = n));
+    }
+    result
+}
+
+#[allow(dead_code)]
+fn main()
 {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
-    let n: usize = input.trim().parse().unwrap();
-
-    for i in 1..=n 
+    if let Ok(n) = input.trim().parse::<usize>() 
     {
-        println!("{}{}", " ".repeat(n - i), "#".repeat(i));
+        let lines = build_staircase(n);
+        for line in lines 
+        {
+            println!("{}", line);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests 
+{
+    use super::*;
+
+    #[test]
+    fn test_staircase_size_3() 
+    {
+        let expected = vec!
+        [
+            "  #",
+            " ##",
+            "###"
+        ];
+        assert_eq!(build_staircase(3), expected);
+    }
+
+    #[test]
+    fn test_staircase_size_1() 
+    {
+        let expected = vec!["#"];
+        assert_eq!(build_staircase(1), expected);
     }
 }
